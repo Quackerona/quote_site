31e1507dc4dd346edb0d6e566499827437e98693
Quackerona <97374920+Quackerona@users.noreply.github.com>
2025-09-30 18:25:43 +0700

    convert to shuttle

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # What's the quote of 10 minutes?
 
+>[!NOTE]
+>Currently being hosted with Shuttle. URL: https://10minutequote-tdtb.shuttle.app/
+
 "What's the quote of 10 minutes" is a fun little side project made to learn Rust. You can submit a line and it will be shared globally! (That is, if you're quick enough).
 A line can be submitted **BY ONE PERSON** every 10 minutes, so be quick if you want to be heard!
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rocket::{fs::{relative, FileServer}, get, launch, post, routes, tokio::{sync::Mutex, time::Instant, time::Duration}, State};
+use rocket::{fs::{relative, FileServer}, get, post, routes, tokio::{sync::Mutex, time::Instant, time::Duration}, State};
 
 const WAITING_DURATION: u64 = 600;
 
@@ -27,10 +27,12 @@ async fn update_message(message_state: &State<Mutex<MessageState>>, msg: String)
     }
 }
 
-#[launch]
-fn rocket() -> rocket::Rocket<rocket::Build> {
-    rocket::build()
+#[shuttle_runtime::main]
+async fn rocket() -> shuttle_rocket::ShuttleRocket {
+    let rocket = rocket::build()
         .manage(Mutex::new(MessageState{time_till_change: Instant::now() - Duration::from_secs(WAITING_DURATION), message: "\"Default quote\"".to_owned()}))
         .mount("/", FileServer::from(relative!("frontend/dist")))
-        .mount("/api", routes![get_message, update_message])
+        .mount("/api", routes![get_message, update_message]);
+
+    Ok(rocket.into())
 }
