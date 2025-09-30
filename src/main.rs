77b85340da1a92f0b950315de7afdc2058a0e852
Quackerona <97374920+Quackerona@users.noreply.github.com>
2025-09-30 21:47:12 +0700

    changing how timer works

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use rocket::{fs::{relative, FileServer}, get, post, routes, tokio::{sync::Mutex,
 const WAITING_DURATION: u64 = 600;
 
 struct MessageState {
-    time_till_change: Instant,
+    next_change_time: Instant,
     message: String
 }
 
@@ -16,21 +16,23 @@ async fn get_message(message_state: &State<Mutex<MessageState>>) -> String {
 async fn update_message(message_state: &State<Mutex<MessageState>>, msg: String) -> String {
     let mut outer_guard = message_state.lock().await;
 
-    if Instant::now().duration_since(outer_guard.time_till_change) >= Duration::from_secs(WAITING_DURATION) {
+    let now = Instant::now();
+
+    if now >= outer_guard.next_change_time {
         outer_guard.message = format!("\"{}\"", msg);
-        outer_guard.time_till_change = Instant::now() + Duration::from_secs(WAITING_DURATION);
+        outer_guard.next_change_time = now + Duration::from_secs(WAITING_DURATION);
 
         "Success!".to_owned()
     }
     else {
-        format!("{}{}{}", "Too late! Wait ", ((outer_guard.time_till_change - Instant::now()).as_secs_f32() / 60.0).ceil(), " minutes!")
+        format!("{}{}{}", "Too late! Wait ", ((outer_guard.next_change_time - now).as_secs_f32() / 60.0).ceil(), " minutes!")
     }
 }
 
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build()
-        .manage(Mutex::new(MessageState{time_till_change: Instant::now() - Duration::from_secs(WAITING_DURATION), message: "\"Default quote\"".to_owned()}))
+        .manage(Mutex::new(MessageState{next_change_time: Instant::now(), message: "\"Default quote\"".to_owned()}))
         .mount("/", FileServer::from(relative!("frontend/dist")))
         .mount("/api", routes![get_message, update_message]);
 
